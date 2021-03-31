120e2256e2dee0a651f20d7d6885a3b93171e4ad
Muhammad Aditya Hilmy <mhilmy@hey.com>
2021-03-31 20:34:27 +0700

    Add ga pageview

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { routes } from "./routes";
 import "./App.css";
 
 ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID || "");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const theme = createMuiTheme({
   palette: {
