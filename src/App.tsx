68b8056e1fd2679e718c8b3470c2e39ccc05b7e5
Muhammad Aditya Hilmy <mhilmy@hey.com>
2021-03-31 20:27:46 +0700

    Add GA

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import ReactGA from "react-ga";
 
 import { routes } from "./routes";
 import "./App.css";
+
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID || "");
 
 const theme = createMuiTheme({
   palette: {
